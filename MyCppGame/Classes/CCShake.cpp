28acac7c439d1672a05a4f0273ced2cbe1f0f037
毛善辉 <503296211@qq.com>
2015-10-05 10:46:33 +0800

    1.0.1

---
+++ MyCppGame/Classes/CCShake.cpp
@@ -0,0 +1,9 @@
+//
+//  CCShake.cpp
+//  MyCppGame
+//
+//  Created by 毛善辉 on 15/10/5.
+//
+//
+
+#include "CCShake.hpp"
---
+++ MyCppGame/Classes/CCShake.hpp
@@ -0,0 +1,14 @@
+//
+//  CCShake.hpp
+//  MyCppGame
+//
+//  Created by 毛善辉 on 15/10/5.
+//
+//
+
+#ifndef CCShake_hpp
+#define CCShake_hpp
+
+#include <stdio.h>
+
+#endif /* CCShake_hpp */
